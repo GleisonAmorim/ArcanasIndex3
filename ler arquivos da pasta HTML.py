5a50a49e8c33b0854b80cc2c1dfdcc424e755c4c
Gleison Amorim <54336609+GleisonAmorim@users.noreply.github.com>
2025-06-15 08:40:51 -0300

    Delete ler arquivos da pasta HTML.py

--- ler arquivos da pasta HTML.py
+++
@@ -1,9 +0,0 @@
-import os
-
-caminho = r'D:\ArcanasIndex-master\NOVOS CORRIGIDOS SUBSTITUIR'
-
-for raiz, pastas, arquivos in os.walk(caminho):
-    for arquivo in arquivos:
-        if arquivo.lower().endswith('.html'):
-            caminho_completo = os.path.join(raiz, arquivo)
-            print(caminho_completo)
